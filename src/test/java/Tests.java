8f0dd81fcf9d0ffd5dce2e72cb7089e7ee37134e
victor.luna <victor.luna@hibrido.com.br>
2022-12-01 17:28:15 -0300

    fix imports on tests

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,13 +1,11 @@
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
 @TestInstance(Lifecycle.PER_CLASS)
